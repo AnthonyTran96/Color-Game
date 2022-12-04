c8152e79a9754a532fa0f4f792eab604347936c2
Anthony Tran <quanghust96@gmail.com>
2022-12-05 01:09:40 +0700

    code gan hoan thien

--- color-game.js
+++ color-game.js
@@ -1,7 +1,9 @@
 window.onload=function(){
+    let result;
     const addColor='addcolor';
+    let RBGColor=document.querySelector('.RBGColor');
     let rgb=document.querySelector('.rgb');
-    let play=document.querySelector('.newColor');
+    let newColor=document.querySelector('.newColor');
     let easy=document.querySelector('.easy');
     let hard=document.querySelector('.hard');
     let tryAgain=document.querySelector('.tryAgain');
@@ -15,9 +17,6 @@ window.onload=function(){
             rgb[2]=Math.floor(Math.random()*256);
         }
         return `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`
-    }
-    let createBlackColor=function(){
-        return `rgb(0, 0, 0)`
     }
     let makeRowColor=function(row){
         row.forEach(function(box,index,row){
@@ -34,39 +33,68 @@ window.onload=function(){
             box.style.backgroundColor=boxColor;
         });
     }
-    let makeBlackRowColor=function(row){
-        row.forEach(function(box){
-            box.style.backgroundColor=createBlackColor();
-        })
-    }
     let resetRgb=function(){
-        let result=createColor();
+        result=createColor();
         rgb.textContent=result;
+        newColor.textContent='new color';
+        tryAgain.textContent='';
+        RBGColor.style.backgroundColor='#2C8E99';
     }
     let rowBox1=Array.from(row1.querySelectorAll('.box'));
     let rowBox2=Array.from(row2.querySelectorAll('.box'));
+    let addResult=function(rowBox){
+        let resultNumber=Math.floor(Math.random()*rowBox.length);
+        rowBox[resultNumber].style.backgroundColor=result;
+    }
     let addEasyLevel=function(){
+        resetRgb();
         makeRowColor(rowBox1);
+        addResult(rowBox1);
         row2.remove();
         easy.classList.add(addColor);
         hard.classList.remove(addColor);
-        resetRgb();
+        return rowBox1;
     }
     let addHardLevel=function(){
-        if (row1.nextSibling!==1) row1.parentNode.appendChild(row2);
+        resetRgb();
+        row1.parentNode.appendChild(row2);
         let rowBox=[...rowBox1,...rowBox2];
         makeRowColor(rowBox);
+        addResult(rowBox);
         hard.classList.add(addColor);
         easy.classList.remove(addColor);
-        resetRgb();
+        return rowBox;
     }
     let addNewColor=function(){
         resetRgb();
-        if (row1.nextSibling!==1) 
+        let rowBox=[...rowBox1,...rowBox2];
+        makeRowColor(rowBox);
+        addResult(rowBox);
+        return rowBox;
     }
+    let makeBlackBox=function(box){
+        box.style.backgroundColor='rgb(0,0,0)';
+    }
+    let play=function(event){
+        let box=event.target;
+        if(box.style.backgroundColor!==result){
+            makeBlackBox(box);
+            tryAgain.textContent='Try Again';
+            return;
+        }
+        tryAgain.textContent='Correct';
+        newColor.textContent='Play Again';
+        rowBox.forEach(function(box){
+            box.style.backgroundColor=result;
+        })
+        RBGColor.style.backgroundColor=result;
+    };
     easy.addEventListener('click',addEasyLevel);
     hard.addEventListener('click',addHardLevel);
-    play.addEventListener('click',addNewColor);
-    resetRgb();
-    addHardLevel();
+    newColor.addEventListener('click',addNewColor);
+    let rowBox=addHardLevel();
+    rowBox.forEach(function(box,index,rowBox){
+        box.addEventListener('click',play);
+    })
+
 }
